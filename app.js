b9a917ba7043c4551e8538e6b724b378bda24647
Prasoon <pp209738@gmail.com>
2025-01-30 02:22:51 +0530

    regex

--- app.js
+++ app.js
@@ -17,11 +17,21 @@ app.get("/userEqual/:age", async (req, res) => {
     res.send(data);
 })
 
+app.get("/regex", async (req, res) => {
+    let data = await userModel.find({name: {$regex: /^m.*or$/i}})
+    res.send(data);
+})
+
 // not equal => ne
 // less than => le
 // less than and equal => lte
 // greater than => gt
 // greater than and equal => gte
-
+// among a given array values matching => $in: [pass values]
+// not in operator => nin
+// a specific field exists for those users => ({isAdmin: {$exists: true}})
+// and operator => {$and: [{isMarried: false}, {age: {$gte: 30}}]}
+// or operator => {$or: [{isMarried: false}, {age: {$gte: 30}}]}
+// regex => $regex: / query /
 
 app.listen(3000);
---
+++ regex.js
@@ -0,0 +1,6 @@
+// starts with => ^
+// ends with => $ eg - suman - an$
+// in middle of n number of letters => .*
+// case insesitive => /i eg - Harsh - ^h/i
+// starting with 'a' and ending with 'on' - ^a.*on$
+
